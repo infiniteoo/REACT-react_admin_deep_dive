5a43d235790ef5694f8fc2b7db7144b0b44e43c0
Troy Dorman <troydorman@gmail.com>
2021-08-01 11:49:06 -0600

    created PostList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Admin, Resource } from "react-admin";
-import restProvider from 'ra-data-simple-rest';
-import PostList from './components/PostList'
+import restProvider from "ra-data-simple-rest";
+import PostList from "./components/PostList";
 
 function App() {
-  return <Admin dataProvider={restProvider('http://localhost:3000')}>
-    <Resource name="posts" list={PostList}/>
-  </Admin>;
+  return (
+    <Admin dataProvider={restProvider("http://localhost:3000")}>
+      <Resource name="posts" list={PostList} />
+    </Admin>
+  );
 }
 
 export default App;
